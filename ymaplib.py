baccba781c7f438c4fcae635b896aacb06916221
Jan Kundrát <jkt@gentoo.org>
2006-02-27 22:59:18 +0000

    - FETCH response support - Coding style fixes - other tweaks :)
    git-svn-id: svn+ssh://svn.flaska.net/var/svn/trojita/trunk@2 db1c769a-ce0d-0410-92a1-9ec0c70b6fba

--- ymaplib.py
+++ ymaplib.py
@@ -7,9 +7,11 @@ References: IMAP4rev1 - RFC3501
 # Author: Jan Kundrát <jkt@flaska.net>
 # Inspired by the Python's imaplib library.
 
-__version__ = "0.1-pre"
+__version__ = "$Revision: 2 $"
+# $Id: ymaplib.py 2 2006-02-27 22:27:29Z jkt $
+# $HeadURL: file:///home/jkt/svn/trojita/ymaplib/ymaplib.py $
 
-__all__ = ["IMAP_stream", "stream_process", "IMAP_response"]
+__all__ = ["IMAPStream", "IMAPResponse", "StreamProcess", "StreamTCP"]
 
 import re
 
@@ -18,7 +20,7 @@ if __debug__:
 
 CRLF = "\r\n"
 
-class stream_process:
+class StreamProcess:
     """Streamable interface to local process. Supports read(), readline(), write() and flush() methods."""
 
     def __init__(self, command):
@@ -29,7 +31,7 @@ class stream_process:
         self.write = self._w.write
         self.flush = self._w.flush
 
-class stream_TCP:
+class StreamTCP:
     """Streamed TCP/IP connection"""
 
     def __init__(self, host, port):
@@ -43,7 +45,7 @@ class stream_TCP:
         self.flush = self._file.flush
 
 
-class IMAP_response:
+class IMAPResponse:
         """Simple container to hold a response from IMAP server. Storage only, don't expect to get usable methods here :)"""
         def __init__(self):
             self.tagged = False               # was it a tagged response?
@@ -58,29 +60,30 @@ class IMAP_response:
                 s += "Tagged"
             else:
                 s += "Untagged"
-            return s + ", kind: " + self.kind.__repr__() + ', response_code: ' + self.response_code.__repr__() + ", data: " + self.data.__repr__() + ">"
+            return s + ", kind: " + self.kind.__repr__() + ', response_code: ' \
+                   + self.response_code.__repr__() + ", data: " + self.data.__repr__() + ">"
 
 
-class IMAP_stream:
+class IMAPStream:
     """Streamed connection to the IMAP4rev1 compliant IMAP server"""
 
-    class not_implemented(Exception):
+    class NotImplementedError(Exception):
         """Something is not yet implemented"""
 
-    class invalid_response(Exception):
+    class InvalidResponseError(Exception):
         """Invalid, unexpected, malformed or unparsable response.
         Possible reasons might be YMAPlib bug, IMAP server error or connection borkage."""
         pass
 
-    class parse_error(invalid_response):
+    class ParseError(InvalidResponseError):
         """Unable to parse server's response"""
         pass
 
-    class invalid_response_wrong_tag(invalid_response):
+    class InvalidResponseWrongTagError(InvalidResponseError):
         """Response contains unknown tag (comand pipelining is not implemented)"""
         pass
 
-    class unknown_response(invalid_response):
+    class UnknownResponseError(InvalidResponseError):
         """Unknown response from server"""
         pass
 
@@ -114,20 +117,21 @@ class IMAP_stream:
     # initialize data structures for server response parsing
     _re_resp_status_tagged = _make_res('^%s (.*)', _resp_status_tagged)
     _re_resp_status = _make_res('^%s (.*)', _resp_status)
-    _re_resp_server_mailbox_status = _make_res('^%s ?(.*)', _resp_server_mailbox_status)
+    _re_resp_server_mailbox_status = _make_res('^%s ?(.*)',
+                                      _resp_server_mailbox_status)
     _re_resp_mailbox_size = _make_res(r'^(\d+) %s', _resp_mailbox_size)
-    _re_resp_message_status = _make_res(r'^(\d+) %s', _resp_message_status)
+    _re_resp_message_status = _make_res(r'^(\d+) %s ?(.*)', _resp_message_status) # the ' ?(.*)' is here to allow matching of FETCH responses
 
     _re_response_code_single = _make_res('%s', _response_code_single)
     _re_response_code_number = _make_res('%s', _response_code_number)
     _re_response_code_spaces = _make_res('%s', _response_code_spaces)
-    _re_response_code_parenthesized = _make_res('%s', _response_code_parenthesized)
+    _re_response_code_parenthesized = _make_res('%s',
+                                       _response_code_parenthesized)
 
     _re_literal = re.compile(r'{(\d+)}')
 
-
     def __init__(self, stream, debug = 0):
-        self.stream = stream
+        self._stream = stream
         if __debug__:
             self.debug = debug
         else:
@@ -135,17 +139,31 @@ class IMAP_stream:
         self.in_progress = 0
         self.tag_current = 0
 
+    def _read(self, size):
+        """Read size octets from server's output"""
+        buf = self._stream.read(size)
+        if __debug__:
+            if self.debug >= 5:
+                self._log('< %s' % buf)
+        return buf
+
+    def _write(self, data):
+        """Write data to server"""
+        if __debug__:
+            if self.debug >= 5:
+                self._log('> %s' % data)
+        return self._stream.write(data)
 
     def _get_line(self):
         """Get one line of server's output. Based on the method of imaplib's IMAP4 class."""
 
-        line = self.stream.readline()
+        line = self._stream.readline()
         if not line:
-            raise self.invalid_response("socket error: EOF")
+            raise self.InvalidResponseError("socket error: EOF")
 
         # Protocol mandates all lines terminated by CRLF
         if not line.endswith(CRLF):
-            raise self.invalid_response("line doesn't end with CRLF")
+            raise self.InvalidResponseError("line doesn't end with CRLF")
 
         # trim the trailing CRLF
         line = line[:-len(CRLF)]
@@ -154,7 +172,6 @@ class IMAP_stream:
             if self.debug >= 5:
                 self._log('< %s' % line)
         return line
-
 
     def _responses(self):
         """Parse the server's responses. Expects zero or more untagged replies
@@ -174,7 +191,7 @@ class IMAP_stream:
 
     def _parse_line(self, line, current_tag):
         """Parse one line of the response to the IMAP_response object."""
-        response = IMAP_response()
+        response = IMAPResponse()
 
         if line.startswith('* '):
             # Untagged response
@@ -183,17 +200,17 @@ class IMAP_stream:
         elif line.startswith('+ '):
             # Command Continuation Request
             # FIXME :)
-            raise self.not_implemented(line)
+            raise self.NotImplementedError(line)
         elif self.tagged_response.match(line):
             # Tagged response
             if not line.startswith(current_tag + " "):
                 # wrong tag
-                raise self.invalid_response_wrong_tag(current_tag, line)
+                raise self.InvalidResponseWrongTagError(current_tag, line)
             response.tagged = True
-            line = line[len(current_tag)+1:]
+            line = line[len(current_tag) + 1:]
         else:
             # Unparsable response
-            raise self.parse_error(line)
+            raise self.ParseError(line)
 
         if response.tagged:
             test = self._re_resp_status_tagged
@@ -201,9 +218,9 @@ class IMAP_stream:
             test = self._re_resp_status
         response.kind = self._helper_foreach(line, test)[0]
 
-        if not response.kind is None:
+        if response.kind is not None:
             # we should check for optional Response Code
-            line = line[len(response.kind)+1:]
+            line = line[len(response.kind) + 1:]
             if line.startswith('['):
                 # parse the Response Code
                 try:
@@ -218,23 +235,29 @@ class IMAP_stream:
                         code = line[1:last]
                         arguments = None
                     response.response_code = (code, arguments)
-                    line = line[last+2:]
+                    line = line[last + 2:]
                 except ValueError:
                     # line contains "[" but no "]"
-                    raise self.parse_error(line)
+                    raise self.ParseError(line)
             # the rest of the line should be only a string
             response.data = line
         elif not response.tagged:
             for test in (self._re_resp_server_mailbox_status, self._re_resp_mailbox_size, self._re_resp_message_status):
                 (response.kind, r) = self._helper_foreach(line, test)
-                if not (response.kind is None):
+                if response.kind == 'FETCH':
+                    # FETCH response will have two items as the result
+                    try:
+                        response.data = (r.groups()[0], r.groups()[1])
+                    except IndexError:
+                        raise self.ParseError(line)
+                elif response.kind is not None:
                     # we've matched against some command
                     response.data = r.groups()[0]
                     break
 
         if response.kind is None:
             # response kind wasn't detected so far
-            raise self.unknown_response(line)
+            raise self.UnknownResponseError(line)
 
         # now it's time to convert textual data in response.data to something better
         return self._parse_response_data(response)
@@ -252,22 +275,22 @@ class IMAP_stream:
     def _parse_response_code(self, code, line):
         """Parse optional (sect 7.1) response codes"""
 
-        if not self._helper_foreach(code, self._re_response_code_single)[0] is None:
+        if self._helper_foreach(code, self._re_response_code_single)[0] is not None:
             # "[atom]"
             return None
-        elif not self._helper_foreach(code, self._re_response_code_number)[0] is None:
+        elif self._helper_foreach(code, self._re_response_code_number)[0] is not None:
             # "[atom number]"
             return int(line)
-        elif not self._helper_foreach(code, self._re_response_code_parenthesized)[0] is None:
+        elif self._helper_foreach(code, self._re_response_code_parenthesized)[0] is not None:
             # "[atom (foo bar)]"
             if not line.startswith('(') or not line.endswith(')'):
-                raise self.parse_error(line)
+                raise self.ParseError(line)
             buf = line[1:-1].split(' ')
             if buf == ['']:
                 return ()
             else:
                 return tuple(line[1:-1].split(' '))
-        elif not self._helper_foreach(code, self._re_response_code_spaces)[0] is None:
+        elif self._helper_foreach(code, self._re_response_code_spaces)[0] is not None:
             # "[atom foo bar]"
             return tuple(line.split(' '))
         else:
@@ -283,7 +306,7 @@ class IMAP_stream:
                 # RFC specifies the rest of the line to be "human readable text" so we don't have much to do here :)
                 pass
             else:
-                raise self.unknown_response(response)
+                raise self.UnknownResponseError(response)
         else:
             if response.kind in self._resp_status:
                 # RFC specifies the rest of the line to be "human readable text" so we don't have much to do here :)
@@ -298,10 +321,10 @@ class IMAP_stream:
                 try:
                     pos1 = response.data.index('(')
                     pos2 = response.data.index(')')
-                    buf = [tuple(response.data[pos1+1:pos2].split(' '))]
+                    buf = [tuple(response.data[pos1 + 1:pos2].split(' '))]
                 except ValueError:
-                    raise self.parse_error(response)
-                line = response.data[pos2+2:]
+                    raise self.ParseError(response)
+                line = response.data[pos2 + 2:]
                 (s, line) = self._extract_astring(line)
                 buf.append(s)
                 (s, line) = self._extract_string(line)
@@ -311,7 +334,7 @@ class IMAP_stream:
                 (s, line) = self._extract_astring(response.data)
                 response.data = [s]
                 if not line.startswith('(') or not line.endswith(')'):
-                    raise self.parse_error(line)
+                    raise self.ParseError(line)
                 items = line[1:-1].split(' ')
                 buf = {}
                 last = None
@@ -322,87 +345,125 @@ class IMAP_stream:
                         buf[item] = None
                         last = item
                     else:
-                        #buf.append([item])
                         try:
                             buf[last] = int(item)
                         except ValueError:
-                            raise self.parse_error(response)
+                            raise self.ParseError(response)
                         last = None
                 response.data.append(buf)
                 response.data = tuple(response.data)
             elif response.kind == 'SEARCH':
                 items = response.data.split(' ')
-                if items == ['',]:
+                if items == ['']:
                     response.data = ()
                 else:
                     try:
-                        items = map(lambda x: int(x), items)
+                        #items = map(lambda x: int(x), items)
+                        items = map(int, items)
                     except ValueError:
-                        raise seld.parse_error(response)
+                        raise self.ParseError(response)
                     response.data = tuple(items)
             elif response.kind == 'FLAGS':
                 if not response.data.startswith('(') or not response.data.endswith(')'):
-                    raise self.parse_error(line)
+                    raise self.ParseError(line)
                 items = response.data[1:-1].split(' ')
                 response.data = tuple(items)
             elif response.kind == 'FETCH':
-                # FIXME :)
-                raise self.not_implemented(response)
+                # "* number FETCH (data...)"
+                try:
+                    pos1 = response.data[1].index('(')
+                    line = response.data[1][pos1 + 1:]
+                except:
+                    raise self.ParseError(response)
+                response.data = self._parse_parenthesized_line(line)[0]
             else:
-                raise self.not_implemented(response)
+                raise self.NotImplementedError(response)
         return response
 
-    def _str_to_int(self, x):
-        return int(x)
+    def _parse_parenthesized_line(self, line):
+        """Parse parenthesized line into Python data structure"""
+        buf = []
+        limit = 0
+        while limit < 1024:
+            # safety limit for 1024 items on the same level at most
+            limit += 1
+            (s, line) = self._extract_string(line)
+            if s == '(':
+                # nested block
+                (item, line) = self._parse_parenthesized_line(line)
+                #print 'nested returned %s' % item
+                buf.append(item)
+            elif s == ')':
+                # end of nested block
+                break
+            elif line == '':
+                # end of string
+                break
+            else:
+                buf.append(s)
+        return (tuple(buf), line)
 
     def _extract_string(self, string):
         """Extract string, including checks for literals"""
         r = self._re_literal.match(string)
         if r:
             # FIXME literal processing - NEEDS TESTING
-            raise self.not_implemented('literals not implemented', string)
             string = ''
             size = int(r.groups()[0])
-            if self.debug > 5:
+            if self.debug >= 6:
                 self._log('got literal - %d octets' % size)
-            buf = self.sock.read(size)
-            if self.sock.read(2) != CRLF:
-                raise self.parse_error('CRLF after literal missing')
+            buf = self._read(size)
+            string = self._get_line()
             return (buf, string)
         else:
             return self._extract_astring(string)
 
     def _extract_astring(self, string):
         """Extract an astring from string. Astring can't be literal."""
+        string = string.lstrip(' ')
         if string.startswith('"'):
             # quoted string
             try:
                 pos = string.index('"', 1)
                 buf = string[1:pos]
-                string = string[pos+2:]
-                return (buf, string)
+                string = string[pos + 1:]
             except ValueError:
-                raise self.parse_error(string)
+                raise self.ParseError(string)
+        elif string.startswith('(') or string.startswith(')'):
+            # "(" or ")"
+            buf = string[0]
+            string = string[1:]
         else:
             # atom
-            try:
-                pos = string.index(' ')
-                buf = string[:pos]
-                string = string[pos+1:]
-                return (buf, string)
-            except:
-                # no trailing space
+            pos_par = string.find(')')
+            pos_space = string.find(' ')
+            if pos_par == -1 and pos_space == -1:
+                # atom
                 buf = string
                 string = ''
-                return (buf, string)
+            elif pos_par == -1:
+                # no ")", but space found
+                buf = string[:pos_space]
+                string = string[pos_space + 1:]
+            elif pos_space == -1:
+                # no space, but ")" found
+                buf = string[:pos_par]
+                string = string[pos_par + 1:]
+            else:
+                # both space and ")"
+                pos = min(pos_par, pos_space)
+                buf = string[:pos]
+                string = string[pos:]
+        string = string.lstrip(' ')
+        return (buf, string)
 
 
     def _send_command(self, command):
         """Sends a raw command, wrapping it with apropriate tag"""
         self.in_progress += 1
         self.tag_current += 1
-        self.stream.write(self._make_tag() + ' ' + command + CRLF)
-        self.stream.flush()
+        self._write(self._make_tag() + ' ' + command + CRLF)
+        self._stream.flush()
 
 
     def _make_tag(self):
@@ -426,5 +487,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print "Usage: %s path/to/imapd" % sys.argv[0]
         sys.exit()
-    stream = stream_process([sys.argv[1]])
-    c = YMAP(stream, 5)
+    stream = ProcessStream([sys.argv[1]])
+    c = IMAPStream(stream, 5)
+    """debug = 10; import ymaplib; y=ymaplib.IMAPStream(ymaplib.StreamProcess('dovecot --exec-mail imap'), debug); y._parse_line(y._get_line(), None); y._send_command('capability'); y._responses(); y._send_command('examine gentoo.gentoo-user-cs'); y._responses(); y._send_command('select gentoo.gentoo-user-cs'); y._responses(); y._send_command('fetch 1 full'); y._responses(); y._send_command('status inbox ()'); y._responses()"""
