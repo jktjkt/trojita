773df93b189a479c3a54cd2b8c46da3e9c8f0458
Jan Kundrát <jkt@gentoo.org>
2006-05-11 12:50:09 +0000

    reduce exception abuse in IMAPParser.loop()
    git-svn-id: svn+ssh://svn.flaska.net/var/svn/trojita/trunk@71 db1c769a-ce0d-0410-92a1-9ec0c70b6fba

--- ymaplib.py
+++ ymaplib.py
@@ -378,20 +378,22 @@ class IMAPParser:
                                         item[0])
                         else:
                             self._write((' {%d}' % len(item[0])) + CRLF)
+                            is_ok = True
                             try:
-                                try:
-                                    while 1:
-                                        # wait for the continuation request
-                                        response = self._loop_from_server()
-                                        if response.tag == tag_name \
-                                           and response.kind == 'BAD':
-                                            # server doesn't like our request
-                                            # there's no point in trying to continue
-                                            raise UnknownResponseError(response)
-                                except CommandContinuationRequest:
-                                    # a little abuse of exceptions :)
-                                    self._write(item[0])
-                            except UnknownResponseError:
+                                while 1:
+                                    # wait for the continuation request
+                                    # or a notification that it never arrives :)
+                                    response = self._loop_from_server()
+                                    if response.tag == tag_name \
+                                       and response.kind == 'BAD':
+                                        # server doesn't like our request
+                                        # there's no point in trying to continue
+                                        is_ok = False
+                                        break
+                            except CommandContinuationRequest:
+                                # a little abuse of exceptions :)
+                                self._write(item[0])
+                            if not is_ok:
                                 # we don't have to send the rest of the command
                                 break
             self._write(CRLF)
