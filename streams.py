ebd42f607afa2d092e36311f267e6bda79a9cb91
Jan Kundrát <jkt@gentoo.org>
2006-06-05 14:03:47 +0000

    major rewrite that should actually work :)
    git-svn-id: svn+ssh://svn.flaska.net/var/svn/trojita/trunk@90 db1c769a-ce0d-0410-92a1-9ec0c70b6fba

--- streams.py
+++ streams.py
@@ -1,22 +1,74 @@
-"""Support a wide range of different streams.
-
-Stream is a special object that supports several file-object-like methods like
-close(), flush(), read(), readline() and write(). As a bonus :), there is
-a has_data() function that check if you can read from the stream without 
-blocking, and a starttls() which (if supported) switches to the encrypted 
-communication channel.
-"""
+"""Supports a wide range of different streams."""
 
 import os
 import select
 import socket
 from OpenSSL import SSL
 
+class Stream:
+    """Stream object.
+
+Stream is a special object that supports several file-object-like methods like
+close(), flush(), read(), readline() and write(). As a bonus :), there is
+a has_data() function that check if you can read from the stream without 
+blocking, and a starttls() which (if supported) switches to the encrypted 
+communication channel on the fly.
+
+This class is just a template for other classes. They should override
+_close(), _flush(), _has_data(), _read(), _readline(), _starttls() and _write()
+with their own implementation. These names start with underscore to prevent the
+need to redefine docstrings.
+"""
+    def __todo(self):
+        """Default handler for methods that aren't implemented"""
+        raise NotImplementedError("streams.Stream doesn't support this method")
+
+    def close(self):
+        self._close()
+
+    def flush(self):
+        self._flush()
+
+    def has_data(self, timeout=0):
+        """Check if we can read from socket without blocking
+
+Timeout is an optional parameter specifying the maximum time to wait for the 
+result. If omitted or None, there's no timeout - the function will block until
+there is something to read. If timeout is zero, the function will return 
+immediately. Positive floating point value is number of seconds to wait.
+"""
+        self._has_data(timeout)
+
+    def read(self, size=-1):
+        self._read(size)
+
+    def readline(self):
+        self._readline()
+
+    def starttls(self):
+        """Setup a SSL/TLS session over existing connection"""
+        self._starttls()
+
+    def write(self, data):
+        self._write(data)
+
+    close.__doc__ = file.close.__doc__
+    flush.__doc__ = file.flush.__doc__
+    read.__doc__ = file.read.__doc__
+    readline.__doc__ = file.readline.__doc__
+    write.__doc__ = file.write.__doc__
+    _close = __todo
+    _flush = __todo
+    _has_data = __todo
+    _read = __todo
+    _readline = __todo
+    _starttls = __todo
+    _write = __todo
+
 class ProcessStream:
     """Streamable interface to local process.
 
-Supports read(), readline(), write(), flush(), and has_data() methods. Doesn't
-work on Win32 systems due to their lack of poll() functionality on pipes.
+Doesn't work on Win32 systems due to their lack of poll() functionality on pipes.
 """
 
     def __init__(self, command, timeout=-1):
@@ -26,18 +78,16 @@ work on Win32 systems due to their lack of poll() functionality on pipes.
         self._r_poll = select.poll()
         self._r_poll.register(self._r.fileno(), select.POLLIN | select.POLLHUP)
         self.timeout = int(timeout)
-        self.flush = self._w.flush
-        self.read = self._r.read
-        self.readline = self._r.readline
-        self.write = self._w.write
         self.okay = True
 
-    def close(self):
+    def _close(self):
         self._r.close()
         self,_w.close()
 
-    def has_data(self, timeout=None):
-        """Check if we can read from socket without blocking"""
+    def _flush(self):
+        return self._w.flush()
+
+    def _has_data(self, timeout):
         if timeout is None:
             timeout = self.timeout
         polled = self._r_poll.poll(timeout)
@@ -58,12 +108,18 @@ work on Win32 systems due to their lack of poll() functionality on pipes.
         else:
             return False
 
-    def starttls(self):
-        raise NotImplementedError('no TLS on pipes')
+    def _read(self, size):
+        return self._r.read(size)
 
-class TCPStream:
+    def _readline(self):
+        return self._r.readline()
+
+    def _write(self, data):
+        return self._w.write(data)
+
+
+class TCPStream(Stream):
     """Streamed TCP/IP connection"""
-    # FIXME: support everything from ProcessStream
 
     def __init__(self, host, port, timeout=-1):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -74,42 +130,23 @@ class TCPStream:
         self.timeout = int(timeout)
         self._sock.settimeout(timeout)
         self.okay = True
-        self.close = self._sock.close
-        self.flush = self._file.flush
-        self.read = self._file.read
-        self.readline = self._file.readline
-        self.write = self._file.write
 
-    def starttls(self):
-        raise NotImplementedError('starttls() on a pure TCPStream instance')
+    def _close(self):
+        return self._sock.close()
 
-#        else:
-#            s = self._buffer
-#            self._buffer = ''
-#            if len(s):
-#                if size >= 2:
-#                    return s + self._ssl_connection.recv(size - 1)
-#                elif size == 1:
-#                    return s
-#                elif size == 0:
-#                    return ''
-#                else:
-#                    return s + self._ssl_connection.recv(1048576)
-#            else:
-#                return self._ssl_connection.recv(size)
-#######################
-#       else:
-#           buf = []
-#           while 1:
-#               #char = self._ssl_connection.recv(1)
-#               char = self.read(1)
-#               buf = char
-#               if char == '\n':
-#                   break
-#           return ''.join(buf)
+    def _flush(self):
+        return self._file.flush()
 
-    def has_data(self, timeout=None):
-        """Check if we can read from socket without blocking"""
+    def _read(self, size=-1):
+        return self._file.read(size)
+
+    def _readline(self):
+        return self._file.readline()
+
+    def _write(self, data):
+        return self._file.write(data)
+
+    def _has_data(self, timeout=None):
         if timeout is None:
             timeout = self.timeout
         polled = self._r_poll.poll(timeout)
@@ -129,64 +166,64 @@ class TCPStream:
                 return False
         else:
             return False
-#        else:
-#            if (self._ssl_connection.pending() > 0) or len(self._buffer):
-#                return True
-#            #old_timeout = self._sock.gettimeout()
-#            #self._sock.settimeout(0)
-#            res = True
-#            try:
-#                self._buffer = self._ssl_connection.recv(1)
-#            except SSL.WantReadError:
-#                res = False
-#            #self._sock.settimeout(old_timeout)
-#            return res
 
 
 class GenericSSLStream(TCPStream):
     """Generic SSL Stream template"""
+
+    def __todo(self):
+        """Dummy _ssl_*() function"""
+        raise NotImplementedError()
+
+    _ssl_close = __todo
+    _ssl_has_data = __todo
+    _ssl_read = __todo
+    _ssl_readline = __todo
+    _ssl_starttls = __todo
+    _ssl_write = __todo
+
     def __init__(self, host, port, timeout=-1):
         TCPStream.__init__(self, host, port, timeout)
         self.ssl = False
 
-    def close(self):
+    def _close(self):
         if self.ssl:
             return self._ssl_close()
         else:
             return TCPStream.close(self)
 
-    def flush(self):
+    def _flush(self):
         if self.ssl:
-            return self._ssl_close()
+            return self._ssl_flush()
         else:
-            return TCPStream.close(self)
+            return TCPStream.flush(self)
 
-    def has_data(self, timeout=None):
+    def _has_data(self, timeout):
         if self.ssl:
             return self._ssl_has_data(timeout)
         else:
             return TCPStream.has_data(self, timeout)
 
-    def read(self, size=-1):
+    def _read(self, size):
         if self.ssl:
             return self._ssl_read(size)
         else:
             return TCPStream.read(self, size)
 
-    def readline(self):
+    def _readline(self):
         if self.ssl:
             return self._ssl_readline()
         else:
             return TCPStream.readline(self)
 
-    def starttls(self):
+    def _starttls(self):
         if self.ssl:
             raise NotImplementedError("can't initiate SSL/TLS twice")
         else:
             self.ssl = self._ssl_starttls()
             return self.ssl
 
-    def write(self, data):
+    def _write(self, data):
         if self.ssl:
             return self._ssl_write(data)
         else:
@@ -194,33 +231,62 @@ class GenericSSLStream(TCPStream):
 
 
 class OpenSSLStream(GenericSSLStream):
+    """OpenSSL-based stream"""
+
     def _ssl_close(self):
         self._ssl_connection.shutdown()
         self._ssl_connection.sock_shutdown(socket.SHUT_RDWR)
 
     def _ssl_flush(self):
-        # FIXME
-        return self._sock.flush()
+        # FIXME: flush :)
+        return self._file.flush()
 
-    def _ssl_read(self, size=-1):
-        # FIXME: size handling (max & default)
-        return self._ssl_connection.recv(size)
+    def _ssl_has_data(self, timeout):
+        if (self._ssl_connection.pending() > 0) or len(self._buffer):
+            return True
+        old_timeout = self._sock.gettimeout()
+        self._sock.settimeout(0)
+        res = True
+        try:
+            self._buffer = self._ssl_connection.recv(1)
+        except SSL.WantReadError:
+            res = False
+        self._sock.settimeout(old_timeout)
+        return res
+
+    def _ssl_read(self, size):
+        if size == 0:
+            return ''
+        if len(self._buffer):
+            buf = self._buffer
+            self._buffer = ''
+            if size > 1:
+                return buf + self._ssl_connection.recv(size - 1)
+            elif size == 1:
+                return buf
+            else:
+                raise ValueError("No idea how to read %d bytes..." % size)
+        else:
+            return self._ssl_connection.recv(size)
 
     def _ssl_readline(self):
-       buf = []
-       while 1:
-           char = self._ssl_connection.recv(1)
-           buf.append(char)
-           if char == '\n':
-               break
-       return ''.join(buf)
+        buf = [self._buffer]
+        self._buffer = ''
+        while 1:
+            char = self._ssl_connection.recv(1)
+            buf.append(char)
+            if char == '\n':
+                break
+        return ''.join(buf)
 
     def _ssl_starttls(self):
+        # FIXME: add certificate checking etc etc
         self._ssl_context = SSL.Context(SSL.TLSv1_METHOD)
         self._ssl_connection = SSL.Connection(self._ssl_context, self._sock)
         self._ssl_connection.set_connect_state()
         self.ssl = True
         self._buffer = ''
+        return True
 
     def _ssl_write(self, data):
         return self._ssl_connection.sendall(data)
