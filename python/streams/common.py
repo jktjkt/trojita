dc3cd7f68ce892b98602082a038dde0d5b3cdbd8
Jan Kundrát <jkt@gentoo.org>
2007-10-29 14:26:41 +0000

    branching off old python stuff
    git-svn-id: svn+ssh://svn.flaska.net/var/svn/trojita/trunk@135 db1c769a-ce0d-0410-92a1-9ec0c70b6fba

--- python/streams/common.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8
-"""Common definitions"""
-
-# Copyright (c) Jan Kundrát <jkt@flaska.net>, 2006 - 2007
-
-__revision__ = "$Id$"
-
-# default timeout in seconds
-default_timeout = 30
