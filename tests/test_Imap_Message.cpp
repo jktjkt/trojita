c3e46f16e381a01d8f42d195c2f0b897ed9bb7a1
Jan Kundrát <jkt@gentoo.org>
2009-04-09 13:14:58 +0200

    fix utf-8

--- tests/test_Imap_Message.cpp
+++ tests/test_Imap_Message.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007 Jan Kundr·t <jkt@gentoo.org>
+/* Copyright (C) 2007 Jan Kundr√°t <jkt@gentoo.org>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
