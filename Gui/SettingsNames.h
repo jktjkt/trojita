cedef89f3c645337929b5c57388c75764ff9a9a4
Jan Kundrát <jkt@gentoo.org>
2009-05-18 00:15:32 +0200

    whitespace

--- Gui/SettingsNames.h
+++ Gui/SettingsNames.h
@@ -7,7 +7,7 @@ namespace Gui {
 
 struct SettingsNames
 {
-    static QString  realNameKey, addressKey;
+    static QString realNameKey, addressKey;
     static QString msaMethodKey, methodSMTP, methodSENDMAIL, smtpHostKey, smtpPortKey,
             smtpAuthKey, smtpUserKey, smtpPassKey, sendmailKey, sendmailDefaultCmd;
     static QString imapMethodKey, methodTCP, methodSSL, methodProcess, imapHostKey,
