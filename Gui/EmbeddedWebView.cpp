5668af57229aa1048814c20433751c1c50b02bcc
Jan Kundrát <jkt@gentoo.org>
2009-09-03 16:59:23 +0200

    Allow copying via Ctrl+C in the message viewer
    This fixes #9.

--- Gui/EmbeddedWebView.cpp
+++ Gui/EmbeddedWebView.cpp
@@ -1,5 +1,6 @@
 #include "EmbeddedWebView.h"
 
+#include <QAction>
 #include <QWebFrame>
 #include <QWebHistory>
 
@@ -23,6 +24,11 @@ EmbeddedWebView::EmbeddedWebView( QWidget* parent, QNetworkAccessManager* networ
     // Scrolling is implemented on upper layers
     page()->mainFrame()->setScrollBarPolicy( Qt::Horizontal, Qt::ScrollBarAlwaysOff );
     page()->mainFrame()->setScrollBarPolicy( Qt::Vertical, Qt::ScrollBarAlwaysOff );
+
+    // Setup shortcuts for standard actions
+    QAction* copyAction = page()->action( QWebPage::Copy );
+    copyAction->setShortcut( tr("Ctrl+C") );
+    addAction( copyAction );
 
     connect( this, SIGNAL(loadFinished(bool)), this, SLOT(handlePageLoadFinished(bool)) );
 
