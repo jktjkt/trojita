f127817efc5d814415475537cc00c9fccd453069
Jan Kundrát <jkt@gentoo.org>
2010-02-16 19:12:22 +0100

    make the header work standalone, too

--- Gui/AbstractPartWidget.h
+++ Gui/AbstractPartWidget.h
@@ -1,6 +1,8 @@
 #ifndef GUI_ABSTRACTPARTWIDGET_H
 #define GUI_ABSTRACTPARTWIDGET_H
 
+#include <QString>
+
 namespace Gui {
 
 /** @short An abstract glue interface providing support for message quoting */
