b912389a10885e7f6ecd4f83587502722097d743
Gil Moskowitz <gmoskowitz@xtuple.com>
2011-01-04 11:07:52 +0100

    XtConnect: use PgSQL-like command line options for the xtconnect-trojita
    This allows the xtconnect-trojita app to use the PostgreSQL environment
    variables or command line options to collect connection information. I've tried
    to use the PG conventions for the command line options:
     -h hostname
     -p portnumber
     -d databasename
     -U username
     -W prompt for a password
    
    The password prompt is sent to standard output and the password is read from
    standard input. If -W has been passed and -U has not, the app also prompts for
    and accepts the username.

--- src/XtConnect/SqlStorage.cpp
+++ src/XtConnect/SqlStorage.cpp
@@ -28,9 +28,12 @@
 */
 
 #include "SqlStorage.h"
+#include <iostream>
+#include <QCoreApplication>
 #include <QCryptographicHash>
 #include <QDebug>
 #include <QSqlError>
+#include <QStringList>
 #include <QTimer>
 #include <QVariant>
 
@@ -47,9 +50,59 @@ SqlStorage::SqlStorage(QObject *parent) :
 
 void SqlStorage::open()
 {
+    QString host;
+    int     port = -1;
+    QString dbname;
+    QString username;
+    QString password;
+    bool    readstdin = false;
+
+    QStringList args = QCoreApplication::arguments();
+    for (int i = 1; i < args.length(); i++)
+    {
+      if (args.at(i) == "-h" && args.length() > i)
+        host = args.at(++i);
+      else if (args.at(i) == "-d" && args.length() > i)
+        dbname = args.at(++i);
+      else if (args.at(i) == "-p" && args.length() > i)
+        port = args.at(++i).toInt();
+      else if (args.at(i) == "-U" && args.length() > i)
+        username = args.at(++i);
+      else if (args.at(i) == "-W")
+        readstdin = true;
+    }
+
+    for (int i = 0; i < 3 && username.isEmpty() && readstdin; i++)
+    {
+      char *rawinput;
+      std::cout << "User: ";
+      std::cin >> rawinput;
+      username = QString(rawinput).trimmed();
+    }
+
+    for (int i = 0; i < 3 && password.isEmpty() && readstdin; i++)
+    {
+      char *rawinput;
+      std::cout << "Password: ";
+      std::cin >> rawinput;
+      password = QString(rawinput).trimmed();
+    }
+
     db = QSqlDatabase::addDatabase( QLatin1String("QPSQL"), QLatin1String("xtconnect-sqlstorage") );
-    db.setUserName( "xtrole" );
-    db.setDatabaseName("xtbatch");
+    if ( ! host.isEmpty() )
+      db.setHostName(host);
+
+    if ( port != -1 )
+      db.setPort(port);
+
+    if ( ! dbname.isEmpty() )
+      db.setDatabaseName( dbname );
+
+    if ( ! username.isEmpty() )
+      db.setUserName( username );
+
+    if ( ! password.isEmpty() )
+      db.setPassword( password );
 
     if ( ! db.open() ) {
         _fail( "Failed to open database connection", db );
