001eda479e181daa37c008d9704452b80d8b8948
Jan Kundrát <jkt@gentoo.org>
2011-01-08 23:54:46 +0100

    Remove obsolete friends

--- src/Imap/Model/Model.h
+++ src/Imap/Model/Model.h
@@ -350,8 +350,6 @@ private:
     friend class ThreadingMsgListModel; // needs access to _taskFactory
 
     friend class IdleLauncher;
-    friend class _MailboxListUpdater;
-    friend class _NumberOfMessagesUpdater;
 
     friend class ImapTask;
     friend class FetchMsgPartTask;
