f6e649e53e929829954ac560259f86400db91b66
Jan Kundrát <jkt@flaska.net>
2012-02-28 22:26:27 +0100

    Better test name

--- tests/tests/test_Imap_Threading/test_Imap_Threading.cpp
+++ tests/tests/test_Imap_Threading/test_Imap_Threading.cpp
@@ -83,7 +83,7 @@ void ImapModelThreadingTest::testStaticThreading_data()
 
     THREAD_LINEAR_THREE_TEN;
 
-    QTest::newRow("no-threads")
+    QTest::newRow("no-threads-ten")
             << QByteArray("(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)")
             << m;
 
