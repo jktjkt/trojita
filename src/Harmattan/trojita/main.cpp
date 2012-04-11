48001a85e801994856f53f40c0e6b006a60bad89
Jan Kundrát <jkt@flaska.net>
2012-04-11 19:13:16 +0200

    qml: initialize app properties properly and bind the QObjects immediately

--- src/Harmattan/trojita/main.cpp
+++ src/Harmattan/trojita/main.cpp
@@ -2,21 +2,25 @@
 #include <QDeclarativeContext>
 #include "qmlapplicationviewer.h"
 #include "ImapAccess.h"
+#include "Common/SetCoreApplication.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QCoreApplication::setApplicationName(QString::fromAscii("trojita"));
+    Common::setCoreApplicationData();
 
     QmlApplicationViewer viewer;
-    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    viewer.setMainQmlFile(QLatin1String("qml/trojita/main.qml"));
-    viewer.showExpanded();
 
     ImapAccess imapAccess;
     QDeclarativeContext *ctxt = viewer.rootContext();
     ctxt->setContextProperty(QLatin1String("imapModel"), imapAccess.model);
     ctxt->setContextProperty(QLatin1String("msgListModel"), imapAccess.msgListModel);
     ctxt->setContextProperty(QLatin1String("mailboxModel"), imapAccess.mailboxModel);
+
+    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
+    viewer.setMainQmlFile(QLatin1String("qml/trojita/main.qml"));
+    viewer.showExpanded();
 
     return app.exec();
 }
