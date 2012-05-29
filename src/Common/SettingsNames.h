99789c6f2ea4fcbf7594122d2b9d57043c695ec2
Jan Kundrát <jkt@flaska.net>
2012-05-29 09:48:24 +0200

    SSL: GUI: ask the user to accept the SSL connection
    refs #71

--- src/Common/SettingsNames.h
+++ src/Common/SettingsNames.h
@@ -33,7 +33,7 @@ struct SettingsNames {
            sendmailKey, sendmailDefaultCmd;
     static QString imapMethodKey, methodTCP, methodSSL, methodProcess, imapHostKey,
            imapPortKey, imapStartTlsKey, imapUserKey, imapPassKey, imapProcessKey,
-           imapStartOffline, imapEnableId;
+           imapStartOffline, imapEnableId, imapSslPemCertificate;
     static QString cacheMetadataKey, cacheMetadataMemory, cacheMetadataPersistent,
            cacheOfflineKey, cacheOfflineNone, cacheOfflineXDays, cacheOfflineXMessages,
            cacheOfflineAll, cacheOfflineNumberDaysKey, cacheOfflineNumberMessagesKey;
