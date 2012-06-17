jktjkt/trojita

commit 0d5fc37a53a6980d768ca727555cc01655cb1fe7

IMAP: QRESYNC: process HIGHESTMODSEQ updates for already selected mailbox

src/Imap/Tasks/KeepMailboxOpenTask.cpp
@@ -769,6 +769,16 @@ bool KeepMailboxOpenTask::handleResponseCodeInsideState(const Imap::Responses::S
         }
         break;
     }
+    case Responses::HIGHESTMODSEQ:
+    {
+        TreeItemMailbox *mailbox = Model::mailboxForSomeItem(mailboxIndex);
+        Q_ASSERT(mailbox);
+        const Responses::RespData<quint64> *const num = dynamic_cast<const Responses::RespData<quint64>* const>(resp->respCodeData.data());
+        Q_ASSERT(num);
+        mailbox->syncState.setHighestModSeq(num->data);
+        model->cache()->setMailboxSyncState(mailbox->mailbox(), mailbox->syncState);
+        return true;
+    }
     default:
         // Do nothing here
         break;

commit fc8138a463a239f23e0017015be278c134b222f0

IMAP: QRESYNC: don't update HIGHESTMODSEQ without updating the UID map too
This fixes an opportunity of inconsistent cache which was introduced in commit
0d5fc37a53a6980d768ca727555cc01655cb1fe7.  A proper fix shall make sure that the
HIGHESTMODSEQ, UID mapping and FLAGS are updated synchronously and only at a
proper time.

src/Imap/Tasks/KeepMailboxOpenTask.cpp
@@ -776,7 +776,8 @@ bool KeepMailboxOpenTask::handleResponseCodeInsideState(const Imap::Responses::S
         const Responses::RespData<quint64> *const num = dynamic_cast<const Responses::RespData<quint64>* const>(resp->respCodeData.data());
         Q_ASSERT(num);
         mailbox->syncState.setHighestModSeq(num->data);
-        model->cache()->setMailboxSyncState(mailbox->mailbox(), mailbox->syncState);
+        // FIXME: set the UID mapping *and* the HIGHESTMODSEQ at once
+        // model->cache()->setMailboxSyncState(mailbox->mailbox(), mailbox->syncState);
         return true;
     }
     default: