jktjkt/trojita

commit abb2e424ca75672b0271ce0e62cdca6c7ed0c9a4

GUI: Disable user interaction with mailboxes that are marked \NoSelect

src/Imap/Model/MsgListModel.cpp
@@ -418,27 +418,18 @@ void MsgListModel::handleRowsInserted(const QModelIndex &parent, int start, int 
 
 void MsgListModel::setMailbox(const QModelIndex &index)
 {
-    waitingForMessages = true;
-    if (!index.isValid()) {
-        msgList = 0;
-        reset();
-        emit mailboxChanged();
+    if (!index.isValid() || !index.data(Imap::Mailbox::RoleMailboxIsSelectable).toBool())
         return;
-    }
+
+    waitingForMessages = true;
 
     const Model *model = 0;
     TreeItemMailbox *mbox = dynamic_cast<TreeItemMailbox *>(Model::realTreeItem(index, &model));
     Q_ASSERT(mbox);
-    if (!mbox->isSelectable()) {
-        msgList = 0;
-        reset();
-        emit mailboxChanged();
-        return;
-    }
     TreeItemMsgList *newList = dynamic_cast<TreeItemMsgList *>(
                                    mbox->child(0, const_cast<Model *>(model)));
     Q_ASSERT(newList);
-    if (newList != msgList && mbox->isSelectable()) {
+    if (newList != msgList) {
         msgList = newList;
         msgList->resetWasUnreadState();
         reset();

src/Imap/Model/MailboxModel.cpp
@@ -215,11 +215,16 @@ Qt::ItemFlags MailboxModel::flags(const QModelIndex &index) const
 
     TreeItemMailbox *mbox = dynamic_cast<TreeItemMailbox *>(static_cast<TreeItem *>(index.internalPointer()));
     Q_ASSERT(mbox);
-    // We can't really disable the Qt::ItemIsSelectable here, as it'd block any user interaction
-    if (mbox->isSelectable() && static_cast<Model *>(sourceModel())->isNetworkAvailable())
-        return Qt::ItemIsDropEnabled | QAbstractProxyModel::flags(index);
-    else
-        return QAbstractProxyModel::flags(index);
+
+    Qt::ItemFlags res = QAbstractProxyModel::flags(index);
+    if (!mbox->isSelectable()) {
+        res &= ~Qt::ItemIsSelectable;
+        res |= Qt::ItemIsEnabled;
+    }
+    if (static_cast<Model *>(sourceModel())->isNetworkAvailable()) {
+        res |= Qt::ItemIsDropEnabled;
+    }
+    return res;
 }
 
 Qt::DropActions MailboxModel::supportedDropActions() const

commit 0d0ba7a78ec79cca43839c47e33beb81fb56f5a4

MsgListModel: reset when deleting the mailbox
This fixes a regression introduced in abb2e424ca75672b0271ce0e62cdca6c7ed0c9a4
-- we would happily ignore calls to setMailbox(QModelIndex()), which is not bad,
but given that the code used exactly that to signal "mailbox is gone", we would
end in a crash.

fixes #523

src/Imap/Model/MsgListModel.cpp
@@ -328,7 +328,9 @@ QMimeData *MsgListModel::mimeData(const QModelIndexList &indexes) const
 
 void MsgListModel::resetMe()
 {
-    setMailbox(QModelIndex());
+    msgListPtr = 0;
+    msgList = QModelIndex();
+    reset();
 }
 
 void MsgListModel::handleRowsAboutToBeRemoved(const QModelIndex &parent, int start, int end)