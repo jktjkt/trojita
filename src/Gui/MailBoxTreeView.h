12583bcd9e9f0715ff4b851c974b2298bb04b2d3
Thomas Gahr <thomas.gahr@physik.uni-muenchen.de>
2012-07-20 21:59:52 +0200

    DropAcionMenu: immitate KDE behaviour, drop unneeded isAccepted-tracking
    * handle modifier keys just as KDE does, Qt's default is dodgy here
    * cleanup code, drop unnecessary tracking of isAccepted in dragMoveEvent
      and dragEnterEvent
    * add license statement

--- src/Gui/MailBoxTreeView.h
+++ src/Gui/MailBoxTreeView.h
@@ -1,23 +1,41 @@
-#ifndef MAILBOXTREEVIEW_H
-#define MAILBOXTREEVIEW_H
+/* Copyright (C) 2012 Thomas Gahr <thomas.gahr@physik.uni-muenchen.de>
+
+   This file is part of the Trojita Qt IMAP e-mail client,
+   http://trojita.flaska.net/
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or the version 3 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef GUI_MAILBOXTREEVIEW_H
+#define GUI_MAILBOXTREEVIEW_H
 
 #include <QTreeView>
 
 namespace Gui{
 
-/** @short subclassed to ask user for desired action upon dropEvent
+/** @short Subclassed for more consistent and intuitive handling of Drag&Drop
 */
 class MailBoxTreeView : public QTreeView
 {
 public:
     MailBoxTreeView();
 protected:
-    void dragEnterEvent(QDragEnterEvent *event);
     void dragMoveEvent(QDragMoveEvent *event);
     void dropEvent(QDropEvent *event);
-private:
-    bool dropMightBeAccepted;
 };
 }
 
-#endif // MAILBOXTREEVIEW_H
+#endif // GUI_MAILBOXTREEVIEW_H
