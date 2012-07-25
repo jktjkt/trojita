53e7364b6100f17a46e9bb308369fa0ed5dff5a2
Jan Kundrát <jkt@flaska.net>
2012-07-25 16:32:30 +0200

    Introduction of the INCTHREAD extension
    refs #514

--- docs/proposed-extensions/build.sh
+++ docs/proposed-extensions/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for DRAFT in draft-imap-qresync-arrived draft-imap-sendmail; do
+for DRAFT in draft-imap-qresync-arrived draft-imap-sendmail draft-imap-incthread; do
     ~/.local/bin/xml2rfc "${DRAFT}.xml" --html --text \
         && ./convert-txt-rfc-to-pdf.sh "${DRAFT}"
 done
