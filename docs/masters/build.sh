d10c4b223df7b7ce3453ca6ccb34c61882e3d52b
Jan Kundrát <jkt@flaska.net>
2012-07-26 12:42:32 +0200

    User BibTeX' proper way of multiple bibliogrpahies

--- docs/masters/build.sh
+++ docs/masters/build.sh
@@ -1,7 +1,5 @@
 TEXDOC=trojita
 
-cat rfc.bib trojita-misc.bib > trojita.bib
-
 pdflatex -shell-escape "${TEXDOC}.tex" && \
     bibtex "${TEXDOC}.aux" && \
     pdflatex -shell-escape "${TEXDOC}.tex" && \
