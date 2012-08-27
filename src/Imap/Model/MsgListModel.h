26297dd95dc47e51f3004247151dfaa1b931b07c
Jan Kundrát <jkt@flaska.net>
2012-08-27 14:42:48 +0200

    qml: escape from the MsgList when the index disappears

--- src/Imap/Model/MsgListModel.h
+++ src/Imap/Model/MsgListModel.h
@@ -56,7 +56,7 @@ public:
     virtual QMimeData *mimeData(const QModelIndexList &indexes) const;
     virtual Qt::DropActions supportedDropActions() const;
 
-    QModelIndex currentMailbox() const;
+    Q_INVOKABLE QModelIndex currentMailbox() const;
 
     enum { SUBJECT, SEEN, FROM, TO, CC, BCC, DATE, RECEIVED_DATE, SIZE, COLUMN_COUNT };
 
