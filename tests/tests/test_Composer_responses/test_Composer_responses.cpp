9afcbd4503b6d32f99556a8bada42bbac86741e7
Jan Kundrát <jkt@flaska.net>
2012-10-22 18:55:40 +0200

    tests: make sure that bottom quoters are respected

--- tests/tests/test_Composer_responses/test_Composer_responses.cpp
+++ tests/tests/test_Composer_responses/test_Composer_responses.cpp
@@ -176,6 +176,17 @@ void ComposerResponsesTest::testPlainTextFormatting_data()
                                                             "bar\n"
                                                             "<blockquote><span class=\"quotemarks\">&gt; </span>baz</blockquote>");
 
+    QTest::newRow("bottom-quoting") << QString::fromUtf8("Foo bar.\n"
+                                                         "> blesmrt\n"
+                                                         ">> 333")
+                                    << QString::fromUtf8("Foo bar.\n"
+                                                         "<blockquote><span class=\"quotemarks\">&gt; </span>blesmrt\n"
+                                                         "<blockquote><span class=\"quotemarks\">&gt;&gt; </span>333</blockquote></blockquote>")
+                                    << QString::fromUtf8("Foo bar.\n"
+                                                         "<blockquote><span class=\"quotemarks\">&gt; </span>blesmrt\n"
+                                                         "<blockquote><span class=\"quotemarks\">&gt;&gt; </span>333</blockquote></blockquote>");
+
+
     // FIXME: more tests, including the format=flowed bits
     //QTest::newRow("long line") << QString("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.") << QString("ahoj\n");
 }
