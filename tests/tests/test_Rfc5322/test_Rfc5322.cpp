49d1c442cd07698e9c041a3de8a5118d056ca328
Jan Kundrát <jkt@flaska.net>
2012-11-02 19:08:38 +0100

    tests: basic tests for References parsing

---
+++ tests/tests/test_Rfc5322/test_Rfc5322.cpp
@@ -0,0 +1,67 @@
+/*
+   This file is part of the kimap library.
+   Copyright (C) 2007 Tom Albers <tomalbers@kde.nl>
+   Copyright (c) 2007 Allen Winter <winter@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include <QDebug>
+#include <QTest>
+#include "test_Rfc5322.h"
+#include "../headless_test.h"
+#include "Imap/Parser/Rfc5322HeaderParser.h"
+
+Q_DECLARE_METATYPE(QList<QByteArray>)
+
+void Rfc5322Test::initTestCase()
+{
+    qRegisterMetaType<QList<QByteArray> >("QList<QByteArray>");
+}
+
+void Rfc5322Test::testReferences()
+{
+    QFETCH(QByteArray, input);
+    QFETCH(bool, ok);
+    QFETCH(QList<QByteArray>, references);
+
+    Imap::Parser::Rfc5322HeaderParser parser;
+    bool res = parser.parse(input);
+    QCOMPARE(res, ok);
+    if (parser.references != references) {
+        qDebug() << "Actual:" << parser.references;
+        qDebug() << "Expected:" << references;
+    }
+    QCOMPARE(parser.references, references);
+}
+
+void Rfc5322Test::testReferences_data()
+{
+    QTest::addColumn<QByteArray>("input");
+    QTest::addColumn<bool>("ok");
+    QTest::addColumn<QList<QByteArray> >("references");
+
+    QList<QByteArray> refs;
+
+    QTest::newRow("empty-1") << QByteArray() << true << refs;
+    QTest::newRow("empty-2") << QByteArray("  ") << true << refs;
+    QTest::newRow("empty-3") << QByteArray("\r\n  \r\n") << true << refs;
+
+    refs << "foo@bar";
+    QTest::newRow("trivial") << QByteArray("reFerences: <foo@bar>\r\n") << true << refs;
+}
+
+
+TROJITA_HEADLESS_TEST( Rfc5322Test )
---
+++ tests/tests/test_Rfc5322/test_Rfc5322.h
@@ -0,0 +1,38 @@
+/*
+    This file is part of the kimap library.
+
+    Copyright (c) 2007 Allen Winter <winter@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef RFCCODECSTEST_H
+#define RFCCODECSTEST_H
+
+#include <QtCore/QObject>
+
+/** @short Unit tests for RFC5322 parsing */
+class Rfc5322Test : public QObject
+{
+  Q_OBJECT
+private Q_SLOTS:
+
+    void initTestCase();
+    void testReferences();
+    void testReferences_data();
+};
+
+#endif
