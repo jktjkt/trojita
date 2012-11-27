bf678110ab83003f8689dc589c4f0aff3ccaf72b
Chusslove Illich (Часлав Илић) <caslav.ilic@gmx.net>
2012-11-27 21:15:35 +0100

    Fix po2ts PO comment scheme.
    po2ts puts class names from TS file as source reference comments in PO file,
    which may confuse PO processing tools. Therefore post-process POT file
    produced by po2ts to convert source reference into extracted comments,
    and then pre-process translated POs to recover original comments
    before applying ts2po to get translated TS files.

--- Messages.sh
+++ Messages.sh
@@ -8,3 +8,9 @@ rm -f "${podir}/trojita.ts"
 lupdate -silent -recursive src/ -ts "${podir}/trojita.ts"
 ts2po --progress=none --pot "${podir}/trojita.ts" "${podir}/trojita_common.pot"
 rm "${podir}/trojita.ts"
+
+# ts2po puts class names into source reference comments (#:)
+# instead of into extracted comments (#.),
+# and does not extract source references at all.
+# So convert source reference comments into extracted comments.
+sed -i 's/^#:/#./' "${podir}/trojita_common.pot"
