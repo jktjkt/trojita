e10e63291577147b2e515238947b56c21fbc8902
Jan Kundrát <jkt@flaska.net>
2012-11-29 15:44:58 +0100

    Unify the way how we call Trojita

--- src/Gui/Window.cpp
+++ src/Gui/Window.cpp
@@ -1217,7 +1217,7 @@ void MainWindow::slotShowLinkTarget(const QString &link)
 void MainWindow::slotShowAboutTrojita()
 {
     QMessageBox::about(this, trUtf8("About Trojitá"),
-                       trUtf8("<p>This is <b>Trojitá</b>, a Qt IMAP e-mail client</p>"
+                       trUtf8("<p>This is <b>Trojitá</b>, a fast Qt IMAP e-mail client</p>"
                               "<p>Copyright &copy; 2006-2012 Jan Kundrát &lt;jkt@flaska.net&gt;</p>"
                               "<p>More information at http://trojita.flaska.net/</p>"
                               "<p>You are using version %1.</p>").arg(
