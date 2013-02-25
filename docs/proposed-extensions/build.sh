e049c40d16deb015d0eb4d3314edf79cba24af63
Jan Kundrát <jkt@flaska.net>
2013-02-25 23:49:38 +0100

    RFCs: the draft-kundrat-qresync-arrived got abandoned, the idea is to update QRESYNC instead

--- docs/proposed-extensions/build.sh
+++ docs/proposed-extensions/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for DRAFT in draft-kundrat-qresync-arrived draft-kundrat-imap-submit draft-kundrat-incthread; do
+for DRAFT in draft-kundrat-imap-submit draft-kundrat-incthread; do
     ~/.local/bin/xml2rfc "${DRAFT}.xml" --html --text \
         && ./convert-txt-rfc-to-pdf.sh "${DRAFT}"
 done
