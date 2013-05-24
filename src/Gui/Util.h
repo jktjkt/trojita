4da78348f94ba6972b8234c97b33e3a8509c8ffe
Jan Kundrát <jkt@flaska.net>
2013-05-24 16:15:34 +0200

    Add missing copyright details

--- src/Gui/Util.h
+++ src/Gui/Util.h
@@ -1,4 +1,5 @@
 /* Copyright (C) 2006 - 2011 Thomas Gahr <thomas.gahr@physik.uni-muenchen.de>
+   Copyright (C) 2006 - 2013 Jan Kundrát <jkt@flaska.net>
 
    This file is part of the Trojita Qt IMAP e-mail client,
    http://trojita.flaska.net/
