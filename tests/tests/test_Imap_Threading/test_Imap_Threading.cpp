jktjkt/trojita

commit faea6a3132fe78fd25e84e7adeeb3edddb89b46b

threading: delay tree pruning until after the event loop is reached again
This is not really a proper fix (servers who don't do VANISHED will still make
the client suffer), but it's a step in the right direction, and also something
much, much more effective than trying a special-case branch for removal of leaf
nodes.

The idea here is to cut the total amount of times pruneTree()/layoutChanged() is
called in total. I'm worried about using non-zero delay due to the unit tests...

refs #642

src/Imap/Model/ThreadingMsgListModel.cpp
@@ -62,6 +62,10 @@ ThreadingMsgListModel::ThreadingMsgListModel(QObject *parent):
     m_shallBeThreading(false), m_sortTask(0), m_sortReverse(false), m_currentSortingCriteria(SORT_NONE),
     m_searchValidity(RESULT_INVALIDATED)
 {
+    m_delayedPrune = new QTimer(this);
+    m_delayedPrune->setSingleShot(true);
+    m_delayedPrune->setInterval(0);
+    connect(m_delayedPrune, SIGNAL(timeout()), this, SLOT(delayedPrune()));
 }
 
 void ThreadingMsgListModel::setSourceModel(QAbstractItemModel *sourceModel)
@@ -343,20 +347,24 @@ void ThreadingMsgListModel::handleRowsAboutToBeRemoved(const QModelIndex &parent
         it->uid = 0;
         it->ptr = 0;
     }
-    emit layoutAboutToBeChanged();
-    updatePersistentIndexesPhase1();
+    if (!m_delayedPrune->isActive()) {
+        // no such event is pedning
+        updatePersistentIndexesPhase1();
+    }
 }
 
 void ThreadingMsgListModel::handleRowsRemoved(const QModelIndex &parent, int start, int end)
 {
     Q_ASSERT(!parent.isValid());
-
     Q_UNUSED(start);
     Q_UNUSED(end);
+    if (!m_delayedPrune->isActive())
+        m_delayedPrune->start();
+}
 
-    // It looks like this simplified approach won't really fly when model starts to issue interleaved rowsRemoved signals,
-    // as we'll just remove everything upon first rowsRemoved.  I'll just hope that it doesn't matter (much).
-
+void ThreadingMsgListModel::delayedPrune()
+{
+    emit layoutAboutToBeChanged();
     pruneTree();
     updatePersistentIndexesPhase2();
     emit layoutChanged();

src/Imap/Model/ThreadingMsgListModel.h
@@ -182,6 +182,8 @@ public slots:
     void slotIncrementalThreadingAvailable(const Responses::ESearch::IncrementalThreadingData_t &data);
     void slotIncrementalThreadingFailed();
 
+    void delayedPrune();
+
 signals:
     void sortingFailed();
 
@@ -301,6 +303,8 @@ private:
     } ResultValidity;
 
     ResultValidity m_searchValidity;
+
+    QTimer *m_delayedPrune;
 
     friend class ::ImapModelThreadingTest; // needs access to wantThreading();
 };

commit da26fea3f89c89f4f15fca6af4d1136270246a31

Threading: prevent dangling persistent indexes when removing multiple messages
Commit faea6a3132fe78fd25e84e7adeeb3edddb89b46b introduced a regression -- when
multiple messages were selected, marked as deleted and subsequently expunged,
the selection reappeared and included new messages which were now at the same
(or similar?) place. This patch fixes one corner case which might have led to
this behavior.

REVIEW: 110705

src/Imap/Model/ThreadingMsgListModel.cpp
@@ -347,10 +347,6 @@ void ThreadingMsgListModel::handleRowsAboutToBeRemoved(const QModelIndex &parent
         it->uid = 0;
         it->ptr = 0;
     }
-    if (!m_delayedPrune->isActive()) {
-        // no such event is pedning
-        updatePersistentIndexesPhase1();
-    }
 }
 
 void ThreadingMsgListModel::handleRowsRemoved(const QModelIndex &parent, int start, int end)
@@ -365,6 +361,7 @@ void ThreadingMsgListModel::handleRowsRemoved(const QModelIndex &parent, int sta
 void ThreadingMsgListModel::delayedPrune()
 {
     emit layoutAboutToBeChanged();
+    updatePersistentIndexesPhase1();
     pruneTree();
     updatePersistentIndexesPhase2();
     emit layoutChanged();

tests/tests/test_Imap_Threading/test_Imap_Threading.cpp
@@ -537,6 +537,10 @@ void ImapModelThreadingTest::init()
 
     // Setup the threading model
     threadingModel->setUserWantsThreading(true);
+
+    // Deactivate the helper_multipleExpunges slot. We don't want QTestLib to run it,
+    // but I'm too lazy to add an extra QObject just for the slot.
+    helper_multipleExpunges_hit = -1;
 }
 
 /** @short Walk the model and output a THREAD-like responsde with the UIDs */
@@ -1267,6 +1271,68 @@ void ImapModelThreadingTest::testRemovingRootWithThreadingInFlight()
     QCOMPARE(threadingModel->rowCount(QModelIndex()), 2);
     verifyIndexMap(buildIndexMap(mapping), mapping);
     cEmpty();
+}
+
+/** @short Check that multiple messages being removed at once doesn't break stuff */
+void ImapModelThreadingTest::testMultipleExpunges()
+{
+    initialMessages(4);
+    Mapping mapping;
+    mapping["0"] = 1;
+    mapping["0.0"] = 0;
+    mapping["1"] = 2;
+    mapping["1.0"] = 0;
+    mapping["2"] = 3;
+    mapping["2.0"] = 0;
+    mapping["3"] = 4;
+    mapping["3.0"] = 0;
+    mapping["4"] = 0;
+    cClient(t.mk("UID THREAD REFS utf-8 ALL\r\n"));
+    cServer(QByteArray("* THREAD (1)(2)(3)(4)\r\n") + t.last("OK thread\r\n"));
+    verifyMapping(mapping);
+    QCOMPARE(threadingModel->rowCount(QModelIndex()), 4);
+    verifyIndexMap(buildIndexMap(mapping), mapping);
+    QCOMPARE(treeToThreading(QModelIndex()), QByteArray("(1)(2)(3)(4)"));
+
+    QPersistentModelIndex m1 = findItem("0");
+    QPersistentModelIndex m2 = findItem("1");
+    QPersistentModelIndex m4 = findItem("3");
+    helper_multipleExpunges_hit = 0;
+
+    QVERIFY(m1.isValid());
+    QVERIFY(m2.isValid());
+    QVERIFY(!helper_indexMultipleExpunges_1.isValid());
+    QVERIFY(m4.isValid());
+
+    // The tricky part is here. The bug we want to test for was this: some code within QItemSelectionModel (?)
+    // grabbed a new persistent index most likely within a slot tied to layoutAboutToBeChanged signal. This persistent
+    // index was, however, not updated by the delayedPrune method, and therefore it was left dangling. In the GUI, this
+    // was apparent because some items were suddenly getting selected after some other messages were removed, and the visual
+    // position of the now bogous selection was conspicuously similar to the expunged messages.
+    connect(threadingModel, SIGNAL(layoutAboutToBeChanged()), this, SLOT(helper_multipleExpunges()));
+    cServer("* 2 EXPUNGE\r\n* 2 EXPUNGE\r\n");
+    disconnect(threadingModel, SIGNAL(layoutAboutToBeChanged()), this, SLOT(helper_multipleExpunges()));
+    QCOMPARE(helper_multipleExpunges_hit, 1);
+
+    QCOMPARE(QString::fromUtf8(treeToThreading(QModelIndex())), QString::fromUtf8("(1)(4)"));
+    QCOMPARE(threadingModel->rowCount(QModelIndex()), 2);
+    QVERIFY(m1.isValid());
+    QVERIFY(!m2.isValid());
+    QVERIFY(!helper_indexMultipleExpunges_1.isValid());
+    QVERIFY(m4.isValid());
+
+    cEmpty();
+}
+
+void ImapModelThreadingTest::helper_multipleExpunges()
+{
+    if (helper_multipleExpunges_hit == -1) {
+        // hack: don't let the QTestLib "run" this method in a standalone manner
+        return;
+    }
+    helper_indexMultipleExpunges_1 = findItem("2");
+    QVERIFY(helper_indexMultipleExpunges_1.isValid());
+    ++helper_multipleExpunges_hit;
 }
 
 TROJITA_HEADLESS_TEST( ImapModelThreadingTest )

tests/tests/test_Imap_Threading/test_Imap_Threading.h
@@ -43,8 +43,11 @@ private slots:
     void testDynamicSearch();
     void testIncrementalThreading();
     void testRemovingRootWithThreadingInFlight();
+    void testMultipleExpunges();
     void testThreadingPerformance();
     void testSortingPerformance();
+
+    void helper_multipleExpunges();
 protected slots:
     virtual void init();
 private:
@@ -59,6 +62,9 @@ private:
     QByteArray numListToString(const QList<uint> &seq);
 
     template<typename T> void reverseContainer(T &container);
+
+    QPersistentModelIndex helper_indexMultipleExpunges_1;
+    int helper_multipleExpunges_hit;
 };
 
 #endif