jktjkt/trojita

commit 6e56db69d0e881cdfcecf885146f738bf1a635a1

html: use Qt's own facilities for HTML escaping
Why didn't we use that from the very beginning?

src/Composer/PlainTextFormatter.cpp
@@ -22,8 +22,9 @@
 */
 
 #include <QObject>
-
-#include <QDebug> // FIXME: jkt: remove this
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+#include <QTextDocument>
+#endif
 
 #include "PlainTextFormatter.h"
 
@@ -78,10 +79,13 @@ QStringList plainTextToHtml(const QString &plaintext, const FlowedFormat flowed)
             static QRegExp quotemarks("^[>\\s]*");
             line.remove(quotemarks);
         }
+        // Escape the HTML entities
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+        line = Qt::escape(line);
+#else
+        line = line.toHtmlEscaped();
+#endif
         // markup *bold*, /italic/, _underline_ and active links
-        line.replace(QString::fromUtf8("§"), QString::fromUtf8("§para;")); // better not clobber these funny characters
-        line.replace(">", "§gt;"); // we cannot escape them after we added actual tags
-        line.replace("<", "§lt;"); // and we cannot use the amps "&" since we'll have to escape it to &amp; later on as well
         line.replace(link, "<a href=\"\\1\">\\1</a>");
         line.replace(mail, "<a href=\"mailto:\\1\">\\1</a>");
 #define MARKUP(_item_) "<span class=\"markup\">"#_item_"</span>"
@@ -89,10 +93,6 @@ QStringList plainTextToHtml(const QString &plaintext, const FlowedFormat flowed)
         line.replace(italic, "\\1<i>" MARKUP(/) "\\2" MARKUP(/) "</i>\\3");
         line.replace(underline, "\\1<u>" MARKUP(_) "\\2" MARKUP(_) "</u>\\3");
 #undef MARKUP
-        line.replace("&", "&amp;");
-        line.replace("§gt;", "&gt;");
-        line.replace("§lt;", "&lt;");
-        line.replace(QString::fromUtf8("§para;"), QString::fromUtf8("§")); // undo the transformation
 
         // if this is a non floating new line, prepend canonical quotemarks
         if (cQuoteLevel && !(cQuoteLevel == quoteLevel && markup.last().endsWith(' '))) {

tests/tests/test_Composer_responses/test_Composer_responses.cpp
@@ -169,9 +169,9 @@ void ComposerResponsesTest::testPlainTextFormatting_data()
             << QString("ahoj <a href=\"http://pwn:123/foo?bar&amp;baz#nope\">http://pwn:123/foo?bar&amp;baz#nope</a>");
     // Test our escaping
     QTest::newRow("escaping-1")
-            << QString::fromUtf8("&gt; § §gt; §para;\n")
-            << QString::fromUtf8("&amp;gt; § §gt; §para;\n")
-            << QString::fromUtf8("&amp;gt; § §gt; §para;\n");
+            << QString::fromUtf8("<>&&gt; § §gt; §para;\n")
+            << QString::fromUtf8("&lt;&gt;&amp;&amp;gt; § §gt; §para;\n")
+            << QString::fromUtf8("&lt;&gt;&amp;&amp;gt; § §gt; §para;\n");
 
     QTest::newRow("mailto-1")
             << QString("ble.smrt-1_2+3@example.org")

commit 97e42ff8e15670fb343169b210a2a23da65b9244

HTML: the regexps for detecting links work on top of *escaped* data
A regression was introduced in 6e56db69. Previously, the HTML escaping was done
via custom placeholders and the regexps therefore had to work on more or less
raw, unescaped data. Since the switch to Qt's own HTML escaping, the regular
expressions see the already-escaped form, and therefore the logic for matching
the special characters have to be changed so that it looks for "&amp;" instead
of "&" ("<", ">" and '"' are not used in these regexps and Qt does not escape
apostrophes ("'")).

Thanks to Caspar for report and for his patch. This commit takes a different
approach without introducing custom "magic strings".

BUG: 323390
REVIEW: 112024

src/Composer/PlainTextFormatter.cpp
@@ -39,20 +39,21 @@ This function recognizes http and https links, e-mail addresses, *bold*, /italic
 */
 QString helperHtmlifySingleLine(QString line)
 {
-    // Static regexps for the engine construction
+    // Static regexps for the engine construction.
+    // Warning, these operate on the *escaped* HTML!
     static QString intro("(^|[\\s\\(\\[\\{])");
     static QString extro("($|[\\s\\),;.\\]\\}])");
     static const QRegExp patternRe(
                 // hyperlinks
                 "(" // cap(1)
                 "https?://" // scheme prefix
-                "[;/?:@=&$\\-_.+!',0-9a-zA-Z%#~\\[\\]\\(\\)\\*]+" // allowed characters
-                "[/@=&$\\-_+'0-9a-zA-Z%#~]" // termination
+                "(?:[;/?:@=$\\-_.+!',0-9a-zA-Z%#~\\[\\]\\(\\)\\*]|&amp;)+" // allowed characters
+                "(?:[/@=$\\-_+'0-9a-zA-Z%#~]|&amp;)" // termination
                 ")"
                 // end of hyperlink
                 "|"
                 // e-mail pattern
-                "([a-zA-Z0-9_\\.!#$%&'\\*\\+\\-/=?^`\\{|\\}~]+@[a-zA-Z0-9\\.\\-_]+)" // cap(2)
+                "((?:[a-zA-Z0-9_\\.!#$%'\\*\\+\\-/=?^`\\{|\\}~]|&amp;)+@[a-zA-Z0-9\\.\\-_]+)" // cap(2)
                 // end of e-mail pattern
                 "|"
                 // formatting markup

tests/tests/test_Html_formatting/test_Html_formatting.cpp
@@ -119,6 +119,13 @@ void HtmlFormattingTest::testPlainTextFormattingViaHtml_data()
     QTest::newRow("escaping-1")
             << QString::fromUtf8("<>&&gt; § §gt; §para;\n")
             << QString::fromUtf8("&lt;&gt;&amp;&amp;gt; § §gt; §para;\n");
+    // A plaintext actually containing some HTML code -- bug 323390
+    QTest::newRow("escaping-html-url-bug-323390")
+            << QString("<a href=\"http://trojita.flaska.net\">Trojita</a>")
+            << QString("&lt;a href=&quot;<a href=\"http://trojita.flaska.net\">http://trojita.flaska.net</a>&quot;&gt;Trojita&lt;/a&gt;");
+    QTest::newRow("escaping-html-mail-bug-323390")
+            << QString("some-mail&ad'dr@foo")
+            << QString("<a href=\"mailto:some-mail&amp;ad'dr@foo\">some-mail&amp;ad'dr@foo</a>");
 
     QTest::newRow("mailto-1")
             << QString("ble.smrt-1_2+3@example.org")