76cc3a1ffb4333f93b480b6412cddb751c09ca9c
Jai Luthra <me@jailuthra.in>
2013-12-09 15:11:00 +0530

    Fix typo in save draft msgbox: s/Safe/Save

--- src/Gui/ComposeWidget.cpp
+++ src/Gui/ComposeWidget.cpp
@@ -202,7 +202,7 @@ void ComposeWidget::closeEvent(QCloseEvent *ce)
     if (!noSaveRequired) {  // save is required
         QMessageBox msgBox(this);
         msgBox.setWindowModality(Qt::WindowModal);
-        msgBox.setWindowTitle(tr("Safe Draft?"));
+        msgBox.setWindowTitle(tr("Save Draft?"));
         QString message(tr("The mail has not been sent.<br>Do you want to save the draft?"));
         if (ui->attachmentsView->model()->rowCount() > 0)
             message += tr("<br><span style=\"color:red\">Warning: Attachments are <b>not</b> saved with the draft!</span>");
