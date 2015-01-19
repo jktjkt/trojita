3a2a348ccbda67caa455362baf0cdd9c27214100
Pali Rohár <pali.rohar@gmail.com>
2015-01-19 13:37:11 +0100

    Update CMake and Windows NSIS installer: include all needed DLL libraries into installer
    CMake code for MSVC and Qt5 libraries is not finished yet.
    Now installer will include only Mingw/gcc and Qt4 libraries.
    
    Change-Id: I6d181b1eaed0a0adb7682468a11af1e9afe21f12

---
+++ cmake/TrojitaNSIS.cmake
@@ -0,0 +1,172 @@
+# Create NSIS Installer, check for all dependent DLL libraries and include them into Installer
+
+### Generate list of files for inclusion into installer ###
+
+# Include trojita executable
+get_target_property(TROJITA_EXE_PATH trojita LOCATION)
+get_filename_component(TROJITA_EXE ${TROJITA_EXE_PATH} NAME)
+file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "!define TROJITA_EXE_PATH \"${TROJITA_EXE_PATH}\"\n!define TROJITA_EXE \"${TROJITA_EXE}\"\n")
+
+# Include trojita icon
+get_filename_component(TROJITA_ICON_PATH ${CMAKE_CURRENT_SOURCE_DIR}/src/icons/trojita.ico REALPATH)
+file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "!define TROJITA_ICON_PATH \"${TROJITA_ICON_PATH}\"\n")
+
+set(TROJITA_INSTALL_FILES "")
+
+set(TROJITA_LIBRARIES "")
+set(TROJITA_LIBRARIES_PATHS "")
+
+if(WITH_QT5)
+    # TODO: List all required Qt5 libraries
+    message(SEND_ERROR "Windows NSIS installer is not supported for Qt5 version yet")
+else()
+    # All Qt4 libraries which are required for Trojita
+    list(APPEND TROJITA_LIBRARIES "${QT_QTMAIN_LIBRARY}" "${QT_QTNETWORK_LIBRARY}" "${QT_QTSQL_LIBRARY}" "${QT_QTGUI_LIBRARY}" "${QT_QTCORE_LIBRARY}" "${QT_QTWEBKIT_LIBRARY}" libsqlite3-0.dll)
+    if(WITH_DBUS)
+        list(APPEND TROJITA_LIBRARIES "${QT_QTDBUS_LIBRARY}" "${QT_QTXML_LIBRARY}" libdbus-1-3.dll)
+    endif()
+endif()
+
+if(WITH_ZLIB)
+    list(APPEND TROJITA_LIBRARIES "${ZLIB_LIBRARIES}")
+endif()
+
+if(PNG_VERSION_STRING)
+    # QtGui depends on libpng, name pattern is: "libpng<maj><min>-<maj><min>.dll"
+    string(REGEX REPLACE "^([0-9])\\.([0-9])\\..*\$" "libpng\\1\\2-\\1\\2.dll" LIBRARY "${PNG_VERSION_STRING}")
+    list(APPEND TROJITA_LIBRARIES "${LIBRARY}")
+endif()
+
+if(CMAKE_COMPILER_IS_GNUCXX)
+    execute_process(COMMAND "${CMAKE_CXX_COMPILER}" -dumpversion OUTPUT_VARIABLE GNU_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(COMMAND "${CMAKE_CXX_COMPILER}" -dumpmachine OUTPUT_VARIABLE GNU_TARGET OUTPUT_STRIP_TRAILING_WHITESPACE)
+    # Detect exceptions handling: http://stackoverflow.com/a/16103497
+    execute_process(COMMAND "${CMAKE_CXX_COMPILER}" -v ERROR_VARIABLE GNU_EXCEPTIONS ERROR_STRIP_TRAILING_WHITESPACE)
+    if(GNU_EXCEPTIONS MATCHES "--enable-[a-z]+-exceptions")
+        string(REGEX REPLACE ".*--enable-([a-z]+)-exceptions.*" "\\1" GNU_EXCEPTIONS "${GNU_EXCEPTIONS}")
+        message(STATUS "Detected ${GNU_EXCEPTIONS} exceptions handling")
+    elseif(NOT GNU_VERSION VERSION_LESS "4.8")
+        set(GNU_EXCEPTIONS seh)
+    else()
+        set(GNU_EXCEPTIONS sjlj)
+    endif()
+    # All not static linked mingw/gcc binaries depends on libstdc++ and GNU runtime library, name pattern is: libgcc_s_<exceptions>-1.dll
+    list(APPEND TROJITA_LIBRARIES libgcc_s_${GNU_EXCEPTIONS}-1.dll libstdc++-6.dll)
+    # Detect gcc install path
+    execute_process(COMMAND "${CMAKE_CXX_COMPILER}" -print-search-dirs OUTPUT_VARIABLE GNU_INSTALL OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(GNU_INSTALL MATCHES "install: ")
+        string(REGEX REPLACE ".*install: ([^\n]+)\n.*" "\\1" GNU_INSTALL "${GNU_INSTALL}")
+        list(APPEND TROJITA_LIBRARIES_PATHS ${GNU_INSTALL})
+    else()
+        # Add additional gcc directories where runtime libraries can be stored if install path is not detected
+        foreach(PREFIX ${CMAKE_SYSTEM_PREFIX_PATH})
+            list(APPEND TROJITA_LIBRARIES_PATHS ${PREFIX}/lib/gcc/${GNU_TARGET}/${GNU_VERSION})
+        endforeach()
+    endif()
+else()
+    # TODO: List all MSVC and CLang required runtime libraries
+    message(SEND_ERROR "Windows NSIS installer is not supported without GCC compiler yet")
+endif()
+
+# Include all DLL libraries
+foreach(LIBRARY ${TROJITA_LIBRARIES})
+    # By default mingw/gcc application are linked with dll.a archives and not dll libraries. In dll.a archive is stored real dll name, but from cmake we cannot read it. Solution is to convert dll.a basename to dll and find new dll file in system.
+    # TODO: Add support for MSVC lib naming scheme
+    if(LIBRARY MATCHES "lib.*\\.dll\\.a\$")
+        if(IS_ABSOLUTE "${LIBRARY}")
+            get_filename_component(LIBRARY ${LIBRARY} NAME)
+        endif()
+        # Convert .dll.a filename to .dll filename
+        string(REGEX REPLACE "^lib(.*\\.dll)\\.a\$" "\\1" LIBRARY "${LIBRARY}")
+    endif()
+    if(NOT IS_ABSOLUTE "${LIBRARY}" AND LIBRARY MATCHES "^.*\\.dll\$")
+        if("${LIBRARY}" STREQUAL "z.dll")
+            # Correct name of libz dll library is zlib1.dll
+            set(LIBRARY "zlib1.dll")
+        endif()
+        find_library(LIBRARY_PATH "${LIBRARY}" PATHS ${TROJITA_LIBRARIES_PATHS} CMAKE_FIND_ROOT_PATH_BOTH)
+        if(LIBRARY_PATH AND NOT LIBRARY_PATH MATCHES "^lib.*\\.dll\\.a\$")
+            set(LIBRARY "${LIBRARY_PATH}")
+        endif()
+        unset(LIBRARY_PATH CACHE)
+    endif()
+    if(LIBRARY MATCHES ".*\\.dll\$")
+        if(IS_ABSOLUTE "${LIBRARY}" AND EXISTS "${LIBRARY}")
+            message(STATUS "Including library ${LIBRARY} into NSIS installer")
+            list(APPEND TROJITA_INSTALL_FILES "${LIBRARY}")
+        else()
+            message(SEND_ERROR "Full path for library ${LIBRARY} was not detected")
+        endif()
+    endif()
+endforeach()
+
+# Include be.contacts executable
+get_target_property(FILE_PATH be.contacts LOCATION)
+list(APPEND TROJITA_INSTALL_FILES "${FILE_PATH}")
+
+# Include common library for plugins
+if(WITH_SHARED_PLUGINS)
+    get_target_property(FILE_PATH Plugins LOCATION)
+    list(APPEND TROJITA_INSTALL_FILES "${FILE_PATH}")
+endif()
+
+# Include all shared plugins
+get_property(SHARED_PLUGINS GLOBAL PROPERTY TROJITA_SHARED_PLUGINS)
+foreach(PLUGIN ${SHARED_PLUGINS})
+    get_target_property(FILE_PATH ${PLUGIN} LOCATION)
+    list(APPEND TROJITA_INSTALL_FILES "${FILE_PATH}")
+endforeach()
+
+# Include additional files
+list(APPEND TROJITA_INSTALL_FILES "${CMAKE_CURRENT_SOURCE_DIR}/README")
+list(APPEND TROJITA_INSTALL_FILES "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+
+# Generate list of install files
+file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "!macro TROJITA_INSTALL_FILES\n")
+if(LinguistForTrojita_FOUND OR Qt5LinguistForTrojita_FOUND)
+    if(num_languages)
+        file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "SetOutPath \"\$INSTDIR\\locale\"\n")
+        file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "File /r /x *.ts /x x_test locale\\\n")
+        file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "SetOutPath \"\$INSTDIR\"\n")
+    endif()
+endif()
+foreach(FILE_PATH ${TROJITA_INSTALL_FILES})
+    file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "File \"${FILE_PATH}\"\n")
+endforeach()
+file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "!macroend\n")
+
+# Generate list of uninstall files
+file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "!macro TROJITA_UNINSTALL_FILES\n")
+if(LinguistForTrojita_FOUND OR Qt5LinguistForTrojita_FOUND)
+    if(num_languages)
+        file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "RMDir /r \"\$INSTDIR\\locale\"\n")
+    endif()
+endif()
+foreach(FILE_PATH ${TROJITA_INSTALL_FILES})
+    get_filename_component(FILE_NAME ${FILE_PATH} NAME)
+    file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "Delete \"\$INSTDIR\\${FILE_NAME}\"\n")
+endforeach()
+file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in "!macroend\n")
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi.in ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi)
+
+### Generate installer ###
+
+if(CMAKE_SIZEOF_VOID_P STREQUAL 4)
+    set(MAKENSIS_OUTPUT Trojita-Setup.exe)
+else()
+    set(MAKENSIS_FLAGS -DX86_64 ${MAKENSIS_FLAGS})
+    set(MAKENSIS_OUTPUT Trojita-Setup-x86_64.exe)
+endif()
+
+if(NOT CMAKE_VERBOSE_MAKEFILE)
+    set(MAKENSIS_FLAGS -V2 -DQUIET ${MAKENSIS_FLAGS})
+endif()
+
+set(MAKENSIS_FLAGS ${MAKENSIS_FLAGS} -NOCD)
+
+add_custom_command(OUTPUT ${MAKENSIS_OUTPUT}
+    COMMAND ${MAKENSIS}
+    ARGS ${MAKENSIS_FLAGS} ${CMAKE_CURRENT_SOURCE_DIR}/packaging/trojita.nsi
+    DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/packaging/trojita.nsi ${CMAKE_CURRENT_BINARY_DIR}/trojita-files.nsi ${TROJITA_EXE_PATH} version)
+add_custom_target(installer ALL DEPENDS ${MAKENSIS_OUTPUT})
