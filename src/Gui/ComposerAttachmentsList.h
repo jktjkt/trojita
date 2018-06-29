e27160df0c9f91c4d776cc41a4e8efd5402f03f4
Jan Kundrát <jkt@kde.org>
2018-06-29 03:00:34 +0200

    Add missing header guards
    Change-Id: I2148001abaa6c4b47bb9c9dc0a8bf9862eb95e8a
    Fixes: https://lgtm.com/projects/g/KDE/trojita/alerts/?mode=list&rule=cpp%2Fmissing-header-guard

--- src/Gui/ComposerAttachmentsList.h
+++ src/Gui/ComposerAttachmentsList.h
@@ -20,6 +20,8 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#pragma once
+
 #include <memory>
 #include <QList>
 #include <QListView>
--- src/Gui/ComposerTextEdit.h
+++ src/Gui/ComposerTextEdit.h
@@ -20,6 +20,8 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#pragma once
+
 class QTimer;
 
 #include <QList>
