jktjkt/trojita

commit 35ff866efb1837b8ba517bd22466c73714035955

Use QByteArray for various IMAP-related paths
Using QString doesn't really make sense in this context because the data we
operate on are absolutely not Unicode.

The change is rather intrusive, though, so I hope that I managed to catch all of
the instances where the drag-and-drop operated on QStrings. Maybe we should
switch to regular URLs here.

src/Imap/Network/MsgPartNetAccessManager.cpp
@@ -73,7 +73,7 @@ QNetworkReply *MsgPartNetAccessManager::createRequest(Operation op, const QNetwo
     }
 
     Q_ASSERT(message.isValid());
-    QModelIndex partIndex = pathToPart(message, req.url().path());
+    QModelIndex partIndex = pathToPart(message, req.url().path().toUtf8());
 
     if (req.url().scheme() == QLatin1String("trojita-imap") && req.url().host() == QLatin1String("msg")) {
         // Internal Trojita reference
@@ -120,21 +120,24 @@ QNetworkReply *MsgPartNetAccessManager::createRequest(Operation op, const QNetwo
 }
 
 /** @short Find a message body part through its slash-separated string path */
-QModelIndex MsgPartNetAccessManager::pathToPart(const QModelIndex &message, const QString &path)
+QModelIndex MsgPartNetAccessManager::pathToPart(const QModelIndex &message, const QByteArray &path)
 {
+    if (!path.startsWith("/"))
+        return QModelIndex();
+
     QModelIndex target = message;
-    QStringList items = path.split('/', QString::SkipEmptyParts);
+    QList<QByteArray> items = path.mid(1).split('/'); // mid(1) to get rid of the leading slash now that we don't use QString::SkipEmptyParts
     bool ok = ! items.isEmpty(); // if it's empty, it's a bogous URL
 
-    for (QStringList::const_iterator it = items.constBegin(); it != items.constEnd(); ++it) {
+    for (QList<QByteArray>::const_iterator it = items.constBegin(); it != items.constEnd(); ++it) {
         int offset = it->toInt(&ok);
         if (!ok) {
             // special case, we have to dive into that funny, irregular special parts now
-            if (*it == QLatin1String("HEADER"))
+            if (*it == "HEADER")
                 target = target.child(0, Imap::Mailbox::TreeItem::OFFSET_HEADER);
-            else if (*it == QLatin1String("TEXT"))
+            else if (*it == "TEXT")
                 target = target.child(0, Imap::Mailbox::TreeItem::OFFSET_TEXT);
-            else if (*it == QLatin1String("MIME"))
+            else if (*it == "MIME")
                 target = target.child(0, Imap::Mailbox::TreeItem::OFFSET_MIME);
             else
                 return QModelIndex();

src/Composer/ComposerAttachments.cpp
@@ -324,7 +324,7 @@ void ImapMessageAttachmentItem::asDroppableMimeData(QDataStream &stream) const
 
 
 ImapPartAttachmentItem::ImapPartAttachmentItem(Model *model, const QString &mailbox, const uint uidValidity, const uint uid,
-                                               const QString &trojitaPath)
+                                               const QByteArray &trojitaPath)
 {
     TreeItemMailbox *mboxPtr = model->findMailboxByName(mailbox);
     if (!mboxPtr)

src/Composer/ComposerAttachments.h
@@ -131,7 +131,7 @@ private:
 /** @short Part of a message stored in an IMAP server */
 class ImapPartAttachmentItem: public AttachmentItem {
 public:
-    ImapPartAttachmentItem(Imap::Mailbox::Model *model, const QString &mailbox, const uint uidValidity, const uint uid, const QString &trojitaPath);
+    ImapPartAttachmentItem(Imap::Mailbox::Model *model, const QString &mailbox, const uint uidValidity, const uint uid, const QByteArray &trojitaPath);
     ~ImapPartAttachmentItem();
 
     virtual QString caption() const;

src/Composer/MessageComposer.cpp
@@ -242,7 +242,7 @@ bool MessageComposer::dropAttachmentList(QDataStream &stream)
             QString mailbox;
             uint uidValidity;
             uint uid;
-            QString trojitaPath;
+            QByteArray trojitaPath;
             if (!validateDropImapPart(stream, mailbox, uidValidity, uid, trojitaPath))
                 return false;
             try {
@@ -347,7 +347,7 @@ bool MessageComposer::dropImapMessage(QDataStream &stream)
 }
 
 /** @short Check that the data representing a single message part are correct */
-bool MessageComposer::validateDropImapPart(QDataStream &stream, QString &mailbox, uint &uidValidity, uint &uid, QString &trojitaPath) const
+bool MessageComposer::validateDropImapPart(QDataStream &stream, QString &mailbox, uint &uidValidity, uint &uid, QByteArray &trojitaPath) const
 {
     stream >> mailbox >> uidValidity >> uid >> trojitaPath;
     if (stream.status() != QDataStream::Ok) {
@@ -378,7 +378,7 @@ bool MessageComposer::dropImapPart(QDataStream &stream)
     QString mailbox;
     uint uidValidity;
     uint uid;
-    QString trojitaPath;
+    QByteArray trojitaPath;
     if (!validateDropImapPart(stream, mailbox, uidValidity, uid, trojitaPath))
         return false;
     if (!stream.atEnd()) {

src/Composer/MessageComposer.h
@@ -98,7 +98,7 @@ private:
     void writeHeaderWithMsgIds(QIODevice *target, const QByteArray &headerName, const QList<QByteArray> &messageIds) const;
 
     bool validateDropImapMessage(QDataStream &stream, QString &mailbox, uint &uidValidity, QList<uint> &uids) const;
-    bool validateDropImapPart(QDataStream &stream, QString &mailbox, uint &uidValidity, uint &uid, QString &trojitaPath) const;
+    bool validateDropImapPart(QDataStream &stream, QString &mailbox, uint &uidValidity, uint &uid, QByteArray &trojitaPath) const;
     bool dropImapMessage(QDataStream &stream);
     bool dropImapPart(QDataStream &stream);
     bool dropAttachmentList(QDataStream &stream);

src/Imap/Model/Cache.h
@@ -117,11 +117,11 @@ public:
     virtual void setMsgFlags(const QString &mailbox, const uint uid, const QStringList &flags) = 0;
 
     /** @short Return part data or a null QByteArray if none available */
-    virtual QByteArray messagePart(const QString &mailbox, const uint uid, const QString &partId) const = 0;
+    virtual QByteArray messagePart(const QString &mailbox, const uint uid, const QByteArray &partId) const = 0;
     /** @short Save data for one message part */
-    virtual void setMsgPart(const QString &mailbox, const uint uid, const QString &partId, const QByteArray &data) = 0;
+    virtual void setMsgPart(const QString &mailbox, const uint uid, const QByteArray &partId, const QByteArray &data) = 0;
     /** @short Drop the data for a message part which is no longer needed */
-    virtual void forgetMessagePart(const QString &mailbox, const uint uid, const QString &partId) = 0;
+    virtual void forgetMessagePart(const QString &mailbox, const uint uid, const QByteArray &partId) = 0;
 
     /** @short Return cached threading info for a given mailbox */
     virtual QVector<Imap::Responses::ThreadingNode> messageThreading(const QString &mailbox) = 0;

src/Imap/Model/CombinedCache.cpp
@@ -119,7 +119,7 @@ void CombinedCache::setMessageMetadata(const QString &mailbox, const uint uid, c
     sqlCache->setMessageMetadata(mailbox, uid, metadata);
 }
 
-QByteArray CombinedCache::messagePart(const QString &mailbox, const uint uid, const QString &partId) const
+QByteArray CombinedCache::messagePart(const QString &mailbox, const uint uid, const QByteArray &partId) const
 {
     QByteArray res = sqlCache->messagePart(mailbox, uid, partId);
     if (res.isEmpty()) {
@@ -128,7 +128,7 @@ QByteArray CombinedCache::messagePart(const QString &mailbox, const uint uid, co
     return res;
 }
 
-void CombinedCache::setMsgPart(const QString &mailbox, const uint uid, const QString &partId, const QByteArray &data)
+void CombinedCache::setMsgPart(const QString &mailbox, const uint uid, const QByteArray &partId, const QByteArray &data)
 {
     if (data.size() < 1024 * 1024) {
         sqlCache->setMsgPart(mailbox, uid, partId, data);
@@ -137,7 +137,7 @@ void CombinedCache::setMsgPart(const QString &mailbox, const uint uid, const QSt
     }
 }
 
-void CombinedCache::forgetMessagePart(const QString &mailbox, const uint uid, const QString &partId)
+void CombinedCache::forgetMessagePart(const QString &mailbox, const uint uid, const QByteArray &partId)
 {
     sqlCache->forgetMessagePart(mailbox, uid, partId);
     diskPartCache->forgetMessagePart(mailbox, uid, partId);

src/Imap/Model/CombinedCache.h
@@ -80,9 +80,9 @@ public:
     virtual QStringList msgFlags(const QString &mailbox, const uint uid) const;
     virtual void setMsgFlags(const QString &mailbox, const uint uid, const QStringList &flags);
 
-    virtual QByteArray messagePart(const QString &mailbox, const uint uid, const QString &partId) const;
-    virtual void setMsgPart(const QString &mailbox, const uint uid, const QString &partId, const QByteArray &data);
-    virtual void forgetMessagePart(const QString &mailbox, const uint uid, const QString &partId);
+    virtual QByteArray messagePart(const QString &mailbox, const uint uid, const QByteArray &partId) const;
+    virtual void setMsgPart(const QString &mailbox, const uint uid, const QByteArray &partId, const QByteArray &data);
+    virtual void forgetMessagePart(const QString &mailbox, const uint uid, const QByteArray &partId);
 
     virtual QVector<Imap::Responses::ThreadingNode> messageThreading(const QString &mailbox);
     virtual void setMessageThreading(const QString &mailbox, const QVector<Imap::Responses::ThreadingNode> &threading);

commit ffca487a79c10f93194db195bc0036bb5ed83fbd

refactor: pathToPart: there's no special handling for empty paths
The comments would try to imply that there's some magic for a path
that's just `/`. There's no such check, so let's remove that comment.

Also, don't talk about QString::SkipEmptyParts which the code used to
use many, many years ago.

Also move the `ok` to where it belongs.

Fixes: 35ff866ef Use QByteArray for various IMAP-related paths

src/Imap/Network/MsgPartNetAccessManager.cpp
@@ -126,11 +126,11 @@ QModelIndex MsgPartNetAccessManager::pathToPart(const QModelIndex &message, cons
         return QModelIndex();
 
     QModelIndex target = message;
-    QList<QByteArray> items = path.mid(1).split('/'); // mid(1) to get rid of the leading slash now that we don't use QString::SkipEmptyParts
-    bool ok = ! items.isEmpty(); // if it's empty, it's a bogous URL
+    QList<QByteArray> items = path.mid(1).split('/'); // mid(1) to get rid of the leading slash
 
     for (QList<QByteArray>::const_iterator it = items.constBegin(); target.isValid() && it != items.constEnd(); ++it) {
         const QAbstractItemModel *model = target.model();
+        bool ok;
         int offset = it->toInt(&ok);
         if (ok)
             target = model->index(offset, 0, target);